0ae154572b772887c61978b384fe0d639cea0d67
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-01-17 17:01:08 +0100

    implemented onBackClick in movie-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,47 +1,26 @@
-import React from "react";
-export class MainView extends React.Component {
+import React from 'react';
 
-/*
-//Import movie Card into MovieView
-import { MovieCard } from '../movie-card/movie-card';
-*/
+export class MovieView extends React.Component {
 
-  //To add a movies state that will host list of movies
-  constructor(){
-    super();
-    this.state = {
-      movies: [
-        { _id: "61dbd174a51ffbd65ec81dab", Title: "Silence of the Lambs", Description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.", ImagePath: "https://www.imdb.com/title/tt0102926/mediaviewer/rm3242988544/?ref_=tt_ov_i"},
-        { _id: "61dbd193a51ffbd65ec81dac", Title: "The Shawshank Redemption", Description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", ImagePath: "https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i"},
-        { _id: "61dbd1b6a51ffbd65ec81dad", Title: "The Green Mile", Description: "The lives of guards on death row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift.", ImagePath: "https://www.imdb.com/title/tt0120689/mediaviewer/rm946247936/?ref_=tt_ov_i"}
-      ]
-    } 
-  }
-  
-render() { 
-
-  this.state = {
-    movies: [
-      { _id: "61dbd174a51ffbd65ec81dab", Title: "Silence of the Lambs", Description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.", ImagePath: "https://www.imdb.com/title/tt0102926/mediaviewer/rm3242988544/?ref_=tt_ov_i"},
-      { _id: "61dbd193a51ffbd65ec81dac", Title: "The Shawshank Redemption", Description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", ImagePath: "https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i"},
-      { _id: "61dbd1b6a51ffbd65ec81dad", Title: "The Green Mile", Description: "The lives of guards on death row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift.", ImagePath: "https://www.imdb.com/title/tt0120689/mediaviewer/rm946247936/?ref_=tt_ov_i"}
-    ]
-  } 
-}
-}
-
-  /*
   render() {
+    const { movie, onBackClick } = this.props;
+
     return (
-      <div className="main-view">
-      <div key={movie._id}>{movie.Title}</div>
-        <div>Silence of the Lambs</div>
-        <div>The Shawshank Redemption</div>
-        <div>The Green Mile</div>
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+
       </div>
     );
   }
 }
-
-export default MainView;
-*/
