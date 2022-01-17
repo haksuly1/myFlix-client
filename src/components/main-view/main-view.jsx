34eea133f83ba8552cda2783f54af943bb1045b7
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-01-17 12:01:09 +0100

    added movies state in main-view-jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,13 +7,13 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+        { _id: "61dbd174a51ffbd65ec81dab", Title: "Silence of the Lambs", Description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.", ImagePath: "https://www.imdb.com/title/tt0102926/mediaviewer/rm3242988544/?ref_=tt_ov_i"},
+        { _id: "61dbd193a51ffbd65ec81dac", Title: "The Shawshank Redemption", Description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", ImagePath: "https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i"},
+        { _id: "61dbd1b6a51ffbd65ec81dad", Title: "The Green Mile", Description: "The lives of guards on death row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift.", ImagePath: "https://www.imdb.com/title/tt0120689/mediaviewer/rm946247936/?ref_=tt_ov_i"}
       ]
-    }
+    } 
   }
-
+  
   render() {
     return (
       <div className="main-view">
@@ -25,4 +25,8 @@ export class MainView extends React.Component {
   }
 }
 
+
 export default MainView;
+
+
+  
