722b0e3495a4fedf3f86f575b7018c96bb7d1347
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-01-20 00:41:47 +0100

    used this.setState to set movie to selectedMovie state

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,26 +1,51 @@
-import React from 'react';
 
-export class MovieView extends React.Component {
+import React from "react";
+
+import { MovieCard } from "../movie-card/movie-card";
+
+import { MovieView } from "../movie-view/movie-view";
+
+export class MainView extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      movies: [
+        { _id: 1, Title: "Silence of the Lambs", Description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.", ImagePath: "https://www.imdb.com/title/tt0102926/mediaviewer/rm3242988544/?ref_=tt_ov_i"},
+        { _id: 2, Title: "The Shawshank Redemption", Description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", ImagePath: "https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i"},
+        { _id: 3, Title: "The Green Mile", Description: "The lives of guards on death row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift.", ImagePath: "https://www.imdb.com/title/tt0120689/mediaviewer/rm946247936/?ref_=tt_ov_i"}
+      ],
+
+      selectedMovie: null
+    };
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
+
 
   render() {
-    const { movie, onBackClick } = this.props;
-
+    const { movies, selectedMovie } = this.state;
+  
+    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+  
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          ))
+        }
       </div>
     );
   }
 }
+
+  export default MainView 
+
+  
+
