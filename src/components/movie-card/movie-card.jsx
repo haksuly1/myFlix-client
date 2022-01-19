7afbf273a6b113e96c314d6f61e2ff15c95264cb
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-01-20 00:38:38 +0100

    calles props in movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export class MovieCard extends React.Component {
   render() {
-    return <div className="movie-card">some title</div>;
+    const { movie } = this.props;
+    
+    return <div className="movie-card">{movie.Title}</div>;
   }
 }
+
