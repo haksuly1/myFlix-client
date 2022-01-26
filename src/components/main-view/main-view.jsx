54f841a462ed56c8d09a252f14daa749410fd01f
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-01-26 22:06:33 +0100

    Fix: installed axios and implemented componentDidMount in main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,24 +1,39 @@
-
 import React from "react";
+import axios from "axios";
 
 import { MovieCard } from "../movie-card/movie-card";
-
 import { MovieView } from "../movie-view/movie-view";
-
-export class MainView extends React.Component{
+export class MainView extends React.Component {
 
   constructor(){
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: "Silence of the Lambs", Description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.", ImagePath: "https://www.imdb.com/title/tt0102926/mediaviewer/rm3242988544/?ref_=tt_ov_i"},
-        { _id: 2, Title: "The Shawshank Redemption", Description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", ImagePath: "https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i"},
-        { _id: 3, Title: "The Green Mile", Description: "The lives of guards on death row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift.", ImagePath: "https://www.imdb.com/title/tt0120689/mediaviewer/rm946247936/?ref_=tt_ov_i"}
-      ],
+      movies: [],
+
+        
+        //{ _id: 1, Title: "Silence of the Lambs", Description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.", ImagePath: "https://www.imdb.com/title/tt0102926/mediaviewer/rm3242988544/?ref_=tt_ov_i"},
+        //{ _id: 2, Title: "The Shawshank Redemption", Description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", ImagePath: "https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i"},
+        //{ _id: 3, Title: "The Green Mile", Description: "The lives of guards on death row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift.", ImagePath: "https://www.imdb.com/title/tt0120689/mediaviewer/rm946247936/?ref_=tt_ov_i"}
+      
+      
 
       selectedMovie: null
-    };
+    }
   }
+
+
+  componentDidMount(){
+    axios.get("https://haksuly1movieapp.herokuapp.com/movies")
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
 
   setSelectedMovie(newSelectedMovie) {
     this.setState({
@@ -29,14 +44,15 @@ export class MainView extends React.Component{
   render() {
     const { movies, selectedMovie } = this.state;
   
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />; //The list is empty!</div>;
   
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+            //(movie) => { this.setSelectedMovie(movie) }}/>
           ))
         }
       </div>
