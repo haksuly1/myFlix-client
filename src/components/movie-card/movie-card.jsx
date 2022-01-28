0955f682051096a2ccd13233682330e66aa1b60d
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-01-28 05:24:05 +0100

    implemented props in movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+import "./movie-card.scss";
 
 export class MovieCard extends React.Component {
+  
   render() {
     const { movie, onMovieClick } = this.props;
     
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    return (
+
+      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>  
+
+    );
   }
 }
 
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Description: PropTypes.string.isRequired,
+      Name: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      BirthYear: PropTypes.number.isRequired
+    }),
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
