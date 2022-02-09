43c977c0d76e0a3c07d5ca0e33507c2e9e72bf2b
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-02-09 01:37:33 +0100

    implemented authentication of users in login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -15,7 +15,7 @@ export function LoginView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     //send a request to the server for athentication
-    axios.post( "https://haksuly1movieapp.herokuapp.com/login", { 
+    axios.post( "https://haksuly1movieapp.herokuapp.com/", { 
       Username: username,
       Password: password
     })
