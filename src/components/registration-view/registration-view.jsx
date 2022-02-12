6b1da384ae78fb7a9a3c1121bb21470e60efec4b
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-02-12 03:41:30 +0100

    edited registration-view.jsx/scss

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 import axios from "axios";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-
 //React Bootstrap
 import { Navbar, Container, Nav, Form, Button, Card, Container } from "react-bootstrap";
-
 //SCSS Import
 import "./registration-view.scss";
 
 export function RegistrationView(props) {
+
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ email, setEmail ] = useState('');
@@ -48,7 +47,7 @@ export function RegistrationView(props) {
     }
 
     if(!birthday){
-      setBirthdayErr("Please engter Birthday");
+      setBirthdayErr("Please enter Birthday");
       isReq = false;
     }
     return isReq;
@@ -76,11 +75,57 @@ export function RegistrationView(props) {
       console.error(response);
       alert("Unable to register user")
     });
-    props.onLoggedIn(username);
+    //props.onLoggedIn(username);
   }
 };
 
   return (
+    <Container>
+    <Row>
+        <Col>
+            <CardGroup>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Register now!</Card.Title>
+                        <Form>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Username:</Form.Label>
+                                <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Enter username" />
+                                {/* code added here to display validation error */}
+                                {usernameErr && <p>{usernameErr}</p>}
+                            </Form.Group>
+
+                            <Form.Group className="mb-3">
+                                <Form.Label>Password:</Form.Label>
+                                <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} minLength="8" placeholder="Your password must be 8 or more characters" />
+                                {/* code added here to display validation error */}
+                                {passwordErr && <p>{passwordErr}</p>}
+                            </Form.Group>
+
+                            <Form.Group className="mb-3">
+                                <Form.Label>Email:</Form.Label>
+                                <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter email" />
+                                {/* code added here to display validation error */}
+                                {emailErr && <p>{emailErr}</p>}
+                            </Form.Group>
+
+                            <Form.Group className="mb-3">
+                                <Form.Label>Birthday:</Form.Label>
+                                <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} placeholder="Enter birthday" />
+                            </Form.Group>
+
+                            <Button variant="outline-light" type="submit" onClick={handleSubmit}>Submit</Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
+        </Col>
+    </Row>
+</Container>
+);
+}
+
+    /*
     <div className="registration-view">
       <Navbar expand="lg" bg="#5B84B1FF" variant="dark" className="registrationNavbar">
       <Container>
@@ -152,13 +197,14 @@ export function RegistrationView(props) {
                </div>
    );
 }
+*/
 
 RegistrationView.propTypes = {
   register: PropTypes.shape({
-    Username: PropTypes.string.isRequired,
-    Password: PropTypes.string.isRequired,
-    Email: PropTypes.string.isRequired,
-    Birthday: PropTypes.string.isRequired
+      Username: PropTypes.string.isRequired,
+      Password: PropTypes.string.isRequired,
+      Email: PropTypes.string.isRequired,
+      Birthday: PropsTypes.string.isRequired,
   }),
   onRegistration: PropTypes.func,
 };
